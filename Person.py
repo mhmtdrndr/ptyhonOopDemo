56b358cee9415a1d03324e5aabb2efc9603cf466
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-07-21 16:35:25 +0300

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+class Person:
+    
+    def __init__(self,id,name,lastName,nationalityId):
+        self.id = id
+        self.name = name
+        self.lastName = lastName
+        self.nationalityId = nationalityId
+        self.eMail = name.lower() + '.' + lastName.lower() + '@company.com'
+    
+    def addPerson(self):
+        
+        personList = []
+        person_dict = {
+            'id': self.id,
+            'name': self.name,
+            'lastName': self.lastName,
+            'nationalityId': self.nationalityId,
+            'e-mail': self.eMail
+        }
+        
+        if not os.path.isfile('person_data.json'):
+            personList.append(person_dict)
+            with open('person_data.json','w') as file:
+                file.write(json.dumps(personList, indent=2))
+        else:
+            with open('person_data.json') as fp:
+                data = json.load(fp)
+            data.append(person_dict)
+            with open('person_data.json','w') as fw:
+                fw.write(json.dumps(data, indent=2))
+                
+        print(f'{self.nationalityId} kimlik numaralı kayıt eklendi')        
+
+        
+    def personList():
+        print(("personel listesi").title())
+        
+        with open('person_data.json') as file:
+            data = json.load(file)
+            for d in data:
+                print(d)
+    
+    def findPerson(id_):
+        if not os.path.isfile('person_data.json'):
+            print('Dosya bulunamadı ...')
+        else:
+            with open('person_data.json') as file:
+                data = json.load(file)
+            for dt in data:
+                if dt["id"] == id_:
+                    print("Kayıt bulundu : ")
+                    print(dt)
+                    break
+            print(f"{id_} id'li kayıt dosyada bulunamadı ...")
+    
+    def updatePerson():
+        pass
+      
+Person.findPerson(4)
